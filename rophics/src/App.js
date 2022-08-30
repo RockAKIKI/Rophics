4add9ec73d0a92e68bdd8047dd02ae6d9dfe0c72
Rock AKIKI <rock.akiki@outlook.com>
2022-08-30 16:19:37 +0200

    initial commit

---
+++ rophics/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+ 
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./7RwH.gif" className="myOpacity" alt="logo" />
+        <p>
+       ROPHYCS
+       </p>
+       <span>
+        This website is dedicated to my own work and develpment it should not be abused in order to connect the people have to connect to my FTP server
+       </span>
+        
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
